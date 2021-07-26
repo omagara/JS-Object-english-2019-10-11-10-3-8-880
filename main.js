66e0d674285725d1001bd09622146ed3ee62903e
omagara <omagara@oocl.com>
2021-07-26 16:01:36 +0800

    Written code for the sum of the fruits

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ user = {name:'John'};
 user.surname = 'Mike';
 user.name = 'Peter';
 delete user.name;
+
+//calculate how many pieces of fruit there are in the fruit object and the output should be 50.
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+    };
+var sum=0;
+for (var val in fruit){
+    sum += fruit[val];
+}
+console.log(sum);
